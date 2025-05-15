9a940dd78c80b077875f987e69966859dcbf04cb
Cesar Melo <csrmds@gmail.com>
2025-05-15 16:36:50 -0300

    bulid para Vercel - correção de logs

--- src/pages/components/PostView.jsx
+++ src/pages/components/PostView.jsx
@@ -182,7 +182,7 @@ export default function PostView(props) {
                     
                         
                     
-                    { images.length > 1 ? (
+                    { images?.length > 1 ? (
                         <>
                             <Slider {...sliderSettins} className=' max-h-192'>
                                 {
@@ -201,7 +201,7 @@ export default function PostView(props) {
                                 }
                             </Slider>
                         </>
-                    ) : images.length == 1 && (
+                    ) : images?.length == 1 && (
                         <>
                             <figure className='flex-none grid content-center h-160'>
                                 <img
@@ -224,18 +224,18 @@ export default function PostView(props) {
                                     
                                 {
                                     //BOTÃO DE LIKE
-                                    likes.length > 0 && likes.find(like => like.user?._id == session?.user?.id) ? (
+                                    likes?.length > 0 && likes.find(like => like.user?._id == session?.user?.id) ? (
                                         <div className='indicator'>
-                                            <div className="indicator-item badge bg-violet-900 border-transparent badge-sm">{likes[0]?._id && likes.length}</div>
+                                            <div className="indicator-item badge bg-violet-900 border-transparent badge-sm">{likes[0]?._id && likes?.length}</div>
                                             <button className="btn btn-sm border-transparent" onClick={()=> unLike(likes.find(like => like.user?._id == session?.user?.id))}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                                     <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
                                                 </svg>
                                             </button>
                                         </div>
-                                    ) : likes.length > 0 ? (
+                                    ) : likes?.length > 0 ? (
                                         <div className='indicator'>
-                                            <div className="indicator-item badge bg-violet-900 border-transparent badge-sm">{likes[0]?._id && likes.length}</div>
+                                            <div className="indicator-item badge bg-violet-900 border-transparent badge-sm">{likes[0]?._id && likes?.length}</div>
                                             <button className="btn btn-sm border-transparent" onClick={likeAdd}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
@@ -253,9 +253,9 @@ export default function PostView(props) {
                                 
                                 {
                                     //BOTÃO DE COMENTÁRIOS
-                                    comments.length > 0 ? (
+                                    comments?.length > 0 ? (
                                         <div className='indicator'>
-                                            <div className="indicator-item badge bg-violet-900 border-transparent badge-sm">{comments[0]?._id && comments.length}</div>
+                                            <div className="indicator-item badge bg-violet-900 border-transparent badge-sm">{comments[0]?._id && comments?.length}</div>
                                             <button className="btn btn-sm border-transparent" onClick={showCommentList}>
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
